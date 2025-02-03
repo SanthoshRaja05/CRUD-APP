5c41d07b021f25f9c9e75cb4db9bfe495c3f753f
SanthoshRaja05 <santhoshraja052004@gmail.com>
2025-02-03 23:09:44 +0530

    server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb://localhost/contacts', {
     useUnifiedTopology: true
 });
 
-// Schema for Contact
+
 const contactSchema = new mongoose.Schema({
     name: String,
     email: String,
@@ -22,26 +22,26 @@ const contactSchema = new mongoose.Schema({
 
 const Contact = mongoose.model('Contact', contactSchema);
 
-// Create new contact
+
 app.post('/contacts', async (req, res) => {
     const contact = new Contact(req.body);
     await contact.save();
     res.json(contact);
 });
 
-// Get all contacts
+
 app.get('/contacts', async (req, res) => {
     const contacts = await Contact.find();
     res.json(contacts);
 });
 
-// Update a contact
+
 app.put('/contacts/:id', async (req, res) => {
     const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true });
     res.json(contact);
 });
 
-// Delete a contact
+
 app.delete('/contacts/:id', async (req, res) => {
     await Contact.findByIdAndDelete(req.params.id);
     res.sendStatus(204);
